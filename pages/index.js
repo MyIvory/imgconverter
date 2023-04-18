ffca91800fa5b37c3b3591aed28b03c19ed9ba1a
MyIvory <pavel.sherban@hotmail.com>
2023-04-18 15:54:09 +0300

    ssl

--- pages/index.js
+++ pages/index.js
@@ -109,7 +109,7 @@ const Home = ({ t }) => {
       formData.append("image", new_file);
 
       try {
-        const response = await fetch("http://www.easytext.gidguns.info/php/test.php", {
+        const response = await fetch("https://www.easytext.gidguns.info/php/test.php", {
           //imgconverter
           //imgtextreader
           method: "POST",
