08ee716c0d7ffc4f1644281dcf8e41cb85d2ea76
MyIvory <pavel.sherban@hotmail.com>
2023-04-20 11:07:45 +0300

    vercel web analitic

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '@/styles/globals.css'
 import 'antd/dist/antd';
 import i18n from '../i18n'
 import { withTranslation } from 'next-i18next'
-
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Component {...pageProps} />
+  
+<Analytics/>
+  </>
 }
 
 export default withTranslation()(App)
